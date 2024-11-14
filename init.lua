69874757ed8ab6eb73361c83bb39b591bc5afeb3
DustyDave961 <144002335+DustyDave961@users.noreply.github.com>
2024-11-14 08:54:43 -0600

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,6 @@
+local modpath = minetest.get_modpath("flowing_concrete")
+
+dofile(modpath .. "/concrete.lua")
+dofile(modpath .. "/textures/flowing_concrete_bucket.png")
+dofile(modpath .. "/textures/flowing_concrete_block.png")
+dofile(modpath .. "/textures/flowing_concrete_wet_cement.png")
